168dfcd81ad29b198c7da37d1f5fbf7ce31f4306
Dhruv Thaker <167208515+dhruvthak3r@users.noreply.github.com>
2025-02-11 20:03:40 +0530

    Delete yojna_yantra directory

--- yojna_yantra/rag_pipeline/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class QueryRequest(BaseModel):
-    query_text: str
-
-class QueryResponse(BaseModel):
-    response_text: str
-    documents: List[dict]
