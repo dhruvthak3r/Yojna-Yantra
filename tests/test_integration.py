564a78ce1e16ffcc5c79901c1fd17234efb313bd
dhruvthak3r <dhruvthaker777@gmail.com>
2025-02-27 20:50:52 +0530

    dumb mistake

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,8 +1,6 @@
-from sentence_transformers import SentenceTransformer
-from Services import query_faiss_index,load_faiss_index
+from Services import query_faiss_index, load_faiss_index
 
-index = load_faiss_index("faiss_index.bin")
-
-indices = query_faiss_index(index, "hello", "sentence-transformers/all-MiniLM-L6-v2")
-
-assert indices is not None
+def test_faiss_query():
+    index = load_faiss_index("faiss_index.bin")
+    indices = query_faiss_index(index, "hello", "sentence-transformers/all-MiniLM-L6-v2")
+    assert indices is not None
