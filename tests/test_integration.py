a9887a44fbc3c5b5216348d44cb3f1b5e936cd60
dhruvthak3r <dhruvthaker777@gmail.com>
2025-03-01 18:17:11 +0530

    changed endpoint-2

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -3,4 +3,4 @@ def test_new_endpoint():
     WEBHOOK_URL = "https://chatbot-492327799816.asia-south1.run.app"
     url = f"{WEBHOOK_URL}/chat.telegram"
     response = requests.get(url)
-    assert response.status_code == 200
+    assert response.status_code is not 200
